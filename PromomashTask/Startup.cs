84c9591ee6d65670a4fe87d293331fc446a2b286
vladbilyk <vlad.bilyk@gmail.com>
2019-10-12 19:01:42 +0300

    Useless code was removed

--- PromomashTask/Startup.cs
+++ PromomashTask/Startup.cs
@@ -49,8 +49,6 @@ namespace PromomashTask
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
