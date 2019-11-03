68d784be1049fa1caf2e0ef5234c3653df2b72e2
vladbilyk <vlad.bilyk@gmail.com>
2019-11-03 19:22:57 +0300

    Switching to async pipes

--- PromomashTask.Client/ClientApp/src/app/components/address/address.component.ts
+++ PromomashTask.Client/ClientApp/src/app/components/address/address.component.ts
@@ -1,15 +1,16 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
-import { FormGroup } from "@angular/forms";
-import { MatSelectChange } from "@angular/material/select";
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { MatSelectChange } from '@angular/material/select';
+import { Observable } from 'rxjs';
 
-import { AddressService } from "../../services/address.service";
-import { Country } from "../../services/Country";
+import { AddressService } from '../../services/address.service';
+import { Country } from '../../services/Country';
 
 @Component({
     changeDetection: ChangeDetectionStrategy.OnPush,
-    selector: "app-address",
-    templateUrl: "./address.component.html",
-    styleUrls: ["./address.component.css"]
+    selector: 'app-address',
+    templateUrl: './address.component.html',
+    styleUrls: ['./address.component.css']
 })
 export class AddressComponent implements OnInit {
 
@@ -21,27 +22,25 @@ export class AddressComponent implements OnInit {
     @Output()
     saved = new EventEmitter<string>();
 
+    countries: Observable<Country[]>;
+
+    provinces: Observable<string[]>;
+
+
     ngOnInit() {
-        this.addressService.getCountries().subscribe(countries => this.countries = countries);
+         this.countries = this.addressService.getCountries();
     }
 
     onSubmit() {
         if (this.address.valid) {
-            const country = this.address.controls["country"].value;
-            const province = this.address.controls["province"].value;
+            const country = this.address.controls['country'].value;
+            const province = this.address.controls['province'].value;
             this.saved.emit(`${country}, ${province}`);
         }
     }
 
-    countries: Country[] = [];
-
-    provinces: string[] = [];
-
     selectCountry(e: MatSelectChange) {
-        this.addressService.getProvinces(e.value.code)
-            .subscribe(provinces => {
-                this.provinces = provinces;
-                this.address.controls["province"].setValue("");
-            });
+        this.address.controls['province'].setValue('');
+        this.provinces = this.addressService.getProvinces(e.value.code);
     }
 }
